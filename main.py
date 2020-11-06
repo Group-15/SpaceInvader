7fe44bcd61dad895f3fd577f41ba0536e35d4227
AngelMaciel23 <amaciel1@student.gsu.edu>
2020-11-05 20:54:37 -0500

    commit

--- main.py
+++ main.py
@@ -1,64 +1,126 @@
 import pygame
+import math
 import random
 
-#Intialize the pygame
+from pygame import mixer
+
+# Intialize the pygame
 pygame.init()
 
-#Creates the window screen
+# Creates the window screen
 screen = pygame.display.set_mode((800, 600))
 
-#Title and Icon for window
+# Background
+background = pygame.image.load('background.png')
+
+# Background Sound
+mixer.music.load('background.wav')
+mixer.music.play(-1)
+
+# Title and Icon for window
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
-#Player
+# Player
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
 playerX_change = 0
 
-#Enemy
-enemyImg = pygame.image.load('enemy.png')
-enemyX = random.randint(0, 800)
-enemyY = random.randint(50, 150)
-enemyX_change = 0.2
-enemyY_change = 40
+# Enemy
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyX.append(random.randint(0, 735))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(1)
+    enemyY_change.append(40)
+
+# missile
+
+# Ready - You can't see the bullet on the screen
+# Fire - The bullet is currently moving
+missileImg = pygame.image.load('missile.png')
+missileX = 0
+missileY = 480
+missileX_change = 0
+missileY_change = 3
+missile_state = "ready"
+
+# Score
+
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+textX = 10
+textY = 10
 
 
-#blit means to draw
+def show_score(x, y):
+    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+# blit means to draw
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
 
-#Infinite Game Loop
+
+def fire_missile(x, y):
+    global missile_state
+    missile_state = "fire"
+    screen.blit(missileImg, (x + 16, y + 10))
+
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt((math.pow(enemyX - missileX, 2)) + (math.pow(enemyY - missileY, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False
+
+
+# Infinite Game Loop
 running = True
 while running:
 
     # RGB- Red, Green, Blue
     screen.fill((0, 0, 0))
-
-
+    # background image
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        #If keystroke is pressed, checks whether it'r right or left
+        # If keystroke is pressed, checks whether it'r right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -5
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 5
+            if event.key == pygame.K_SPACE:
+                if missile_state is "ready":
+                    bullet_Sound = mixer.Sound('laser.wav')
+                    bullet_Sound.play()
+                    missileX = playerX
+                    fire_missile(missileX, missileY)
 
-        #When keystroke is let go
+        # When keystroke is let go
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
-    #Checking for player boundries
+    # Checking for player boundries
     playerX += playerX_change
 
     if playerX <= 0:
@@ -66,16 +128,38 @@ while running:
     elif playerX >= 736:
         playerX = 736
 
-    #Enemy movement
-    enemyX += enemyX_change
+    # Enemy movement
+    for i in range(num_of_enemies):
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 2
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -2
+            enemyY[i] += enemyY_change[i]
 
-    if enemyX <= 0:
-        enemyX_change = 0.2
-        enemyY += enemyY_change
-    elif enemyX >= 736:
-        enemyX_change = -0.2
-        enemyY += enemyY_change
+        # Collision
+        collision = isCollision(enemyX[i], enemyY[i], missileX, missileY)
+        if collision:
+            explosion_Sound = mixer.Sound('explosion.wav')
+            explosion_Sound.play()
+            missileY = 480
+            missile_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 735)
+            enemyY[i] = random.randint(50, 150)
+
+        enemy(enemyX[i], enemyY[i], i)
+
+    # missle movement
+    if missileY <= 0:
+        missileY = 480
+        missile_state = "ready"
+
+    if missile_state is "fire":
+        fire_missile(missileX, missileY)
+        missileY -= missileY_change
 
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
+    show_score(textX, textY)
     pygame.display.update()
